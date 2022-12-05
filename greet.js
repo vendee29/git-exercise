f0a3dc8f9ca84da5fde1351956c588abaccdd91d
Matus Tatarko <matus.tatarko28@gmail.com>
2022-12-05 15:58:32 +0100

    b6 - adding printing out of param name2 again at the end of sentence.

--- greet.js
+++ greet.js
@@ -1,7 +1,7 @@
 console.log('Hello World');
 
 function greet(name1, name2){
-	console.log(`Greetings for the ${name1} and ${name2}.`);
+	console.log(`Greetings for the ${name1} and ${name2} and again ${name2}.`);
 };
 
 greet('me', 'you');
