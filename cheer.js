b35b6863f672b7daa57cea0cc5bddc059920313e
Tomáš Hečko <01tomas01@gmail.com>
2022-12-05 15:16:25 +0100

    Another change in the branch

--- cheer.js
+++ cheer.js
@@ -2,4 +2,6 @@ function cheer(teamName, secondParam) {
     console.log(`Hello ${teamName}, ${secondParam}`);
 
     console.log('This is just to make conflict, lol');
+
+    console.log('Another change lol');
 }
